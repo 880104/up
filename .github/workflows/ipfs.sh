79f1ef6a61508c0a6e842ffd9401003bd3b8c18f
880104 <103015572+880104@users.noreply.github.com>
2022-04-05 16:07:10 +0800

    Update ipfs.sh

--- .github/workflows/ipfs.sh
+++ .github/workflows/ipfs.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
-unxz ipfs.xz || exit 1
+SelfFile=$(realpath ${BASE_SOURCE[0]} || exit 1)
+SelfDir=$(dirname $SelfFile || exit 1)
 
-chmod +x ipfs || exit 2
+cd $SelfDir || exit 1
+
+unxz ipfs.xz || exit 2
+
+chmod +x ipfs || exit 3
 
 ./ipfs
